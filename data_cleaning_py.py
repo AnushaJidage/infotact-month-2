20d902f8e358a8a8eaaa179b641ae367196afa4a
AnushaJidage <anushajidage@gmail.com>
2026-07-16 18:49:38 +0530

    Update data_cleaning_py.py

--- data_cleaning_py.py
+++ data_cleaning_py.py
@@ -119,3 +119,19 @@ cohort_df = pd.merge(
 print(cohort_df.head())
 print(cohort_df.shape)
 print(cohort_df.info())
+# Calculate first purchase month of each customer
+cohort_df['CohortMonth'] = (
+    cohort_df.groupby('customer_unique_id')['order_purchase_timestamp']
+             .transform('min')
+             .dt.to_period('M')
+)
+
+# Verify
+print(
+    cohort_df[['customer_unique_id',
+               'order_purchase_timestamp',
+               'CohortMonth']].head()
+)
+
+# Save dataset
+cohort_df.to_csv("cohort_dataset.csv", index=False)
